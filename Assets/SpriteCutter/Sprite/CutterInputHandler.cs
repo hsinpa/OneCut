c02465a23ae49ab5e3935df8b7e2a8e1300dc561
hsinpa <hsinpa@gmail.com>
2019-01-30 00:18:09 +0800

    Initial Update

---
+++ Assets/SpriteCutter/Sprite/CutterInputHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutterInputHandler : MonoBehaviour {
+
+    private LineRenderer lineRenderer;
+    public Camera camera;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+
+    void Update () {
+ //        var v3 = Input.mousePosition;
+ //v3.z = 10.0;
+ //v3 = Camera.main.ScreenToWorldPoint(v3);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse Down");
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Mouse Up");
+        }
+    }
+
+
+}
